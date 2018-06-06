29a7d66f8f5a66e97753cbbdf8e15da706dae40b
R. López <ramonlpmd@gmail.com>
2018-06-05 21:19:58 -0700

    Refactored config file and added helper function to load config on runtime

---
+++ helper.js
@@ -0,0 +1,10 @@
+function XHR(file, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open('GET', file, false);
+    xhr.send();
+}
