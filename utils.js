26b2b7b1b113c2d956f0dcb969a76af5ac1fd485
R. López <ramonlpmd@gmail.com>
2018-06-07 20:22:43 -0700

    Renamed helper to utils and added an isEmpty function

--- utils.js
+++ utils.js
@@ -8,3 +8,11 @@ function XHR(file, callback) {
     xhr.open('GET', file, true);
     xhr.send();
 }
+
+function isEmpty(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    }
+
+    return !obj || Object.keys(obj).length === 0;
+}
